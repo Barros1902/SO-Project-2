1548d32aecdbee3cf567562af0cddc308d3bc093
sophia <sophia.alencar@sapo.pt>
2023-12-30 20:11:24 +0000

    igual

--- P2_base/proj_23-24-p2_base/client/api.c
+++ P2_base/proj_23-24-p2_base/client/api.c
@@ -3,6 +3,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <unistd.h>
 
 int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
 	int server_pipe_path_, written, req_pipe, resp_pipe;
@@ -15,6 +16,7 @@ int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const*
 	if ((req_pipe = open(req_pipe_path, O_WRONLY)) < 0){
 		exit(1);
 	}
+
 	if ((resp_pipe = open(resp_pipe_path, O_RDONLY)) < 0){
 		exit(1);
 	}
@@ -27,3 +29,29 @@ int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const*
 	if (written == -1){
 		return 1;
 	}
+}
+
+int ems_quit(void) { 
+  //TODO: close pipes
+  return 1;
+}
+
+int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
+  //TODO: send create request to the server (through the request pipe) and wait for the response (through the response pipe)
+  return 1;
+}
+
+int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys) {
+  //TODO: send reserve request to the server (through the request pipe) and wait for the response (through the response pipe)
+  return 1;
+}
+
+int ems_show(int out_fd, unsigned int event_id) {
+  //TODO: send show request to the server (through the request pipe) and wait for the response (through the response pipe)
+  return 1;
+}
+
+int ems_list_events(int out_fd) {
+  //TODO: send list request to the server (through the request pipe) and wait for the response (through the response pipe)
+  return 1;
+}
