ac9a72b876152902351c4aa33e09b9afbc115fb6
Tomas Barros <ist1106588@tecnico.ulisboa.pt>
2023-12-30 20:13:01 +0000

    testing for it included

--- P2_base/proj_23-24-p2_base/client/api.c
+++ P2_base/proj_23-24-p2_base/client/api.c
@@ -3,28 +3,33 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
 
 int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
 	int server_pipe_path_, written, req_pipe, resp_pipe;
-	char* req_len;
-	sprintf(req_len, "%dG", strlen(req_pipe_path));
+	char req_len[10000];
+	sprintf(req_len, "%dG", (int)strlen(req_pipe_path));
 	unlink(req_pipe_path);
 	unlink(resp_pipe_path);
 	mkfifo(req_pipe_path, 0777);
 	mkfifo(resp_pipe_path, 0777);
-	if ((req_pipe = open(req_pipe_path, O_WRONLY)) < 0){
-		exit(1);
+	if (!(req_pipe = open(req_pipe_path, O_WRONLY))){
+		return 1;
 	}
 
-	if ((resp_pipe = open(resp_pipe_path, O_RDONLY)) < 0){
-		exit(1);
+	if (!(resp_pipe = open(resp_pipe_path, O_RDONLY))){
+		return 1;
 	}
 	
 
 	if (!(server_pipe_path_ = open(server_pipe_path, O_WRONLY))){
 		return 1;
 	}
-	written = write(server_pipe_path_, strcat(req_len, strcat(req_pipe_path, resp_pipe_path)), strlen(req_len) + strlen(req_pipe_path) + strlen(resp_pipe_path) + 3);
+	printf("%s - req_pipe\n%s - resp_pipe\n%s - reqln\n", req_pipe_path, resp_pipe_path, req_len);
+	scanf("%d", &written);
+	written = write(server_pipe_path_, strcat(strcat(req_len, req_pipe_path), resp_pipe_path), strlen(req_len) + strlen(req_pipe_path) + strlen(resp_pipe_path) + 1);
 	if (written == -1){
 		return 1;
 	}
